b8ad30cd1dea91b5f8565ab42501af6a6898e58c
Ben Romine <elementnumber35@gmail.com>
2019-01-17 15:21:09 -0500

    first commit

---
+++ src/main/java/com/cs4500sp19s1bromine/prototype/PrototypeApplication.java
@@ -0,0 +1,14 @@
+package com.cs4500sp19s1bromine.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeApplication.class, args);
+    }
+
+}
+
