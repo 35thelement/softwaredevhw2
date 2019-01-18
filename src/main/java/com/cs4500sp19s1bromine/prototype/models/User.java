fba83e77d23fb6224b10c599fecf22b54b8160f6
Ben Romine <elementnumber35@gmail.com>
2019-01-18 10:32:32 -0500

    added constructor and getters/setters for each field

--- src/main/java/com/cs4500sp19s1bromine/prototype/models/User.java
+++ src/main/java/com/cs4500sp19s1bromine/prototype/models/User.java
@@ -9,11 +9,42 @@ public class User {
     // provide a public set and get method for each of the class variables above
     // make sure to use proper naming conventions for Java methods, e.g.,
     // methods should be camelcase. Here's an example using the firstName variable:
+    public User(int id, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public String getFirstName() {
         return firstName;
     }
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
 
